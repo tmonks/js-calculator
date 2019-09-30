1b798caf4f7d90bcdcc1fb82e10a9e3280d9455b
Tom Monks <monksy@gmail.com>
2019-09-30 05:00:21 -0400

    Updates

---
+++ precache-manifest.ce26d572833816092bae4523451a0214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f67b72e1e3b29e62239eb6b24ed862b",
+    "url": "/js-calculator/index.html"
+  },
+  {
+    "revision": "969352537875ac5af6de",
+    "url": "/js-calculator/static/css/main.b4ee25be.chunk.css"
+  },
+  {
+    "revision": "c0ebf9f42ed489c6edbb",
+    "url": "/js-calculator/static/js/2.873c1d54.chunk.js"
+  },
+  {
+    "revision": "969352537875ac5af6de",
+    "url": "/js-calculator/static/js/main.dfe905cf.chunk.js"
+  },
+  {
+    "revision": "16dffeded061212ad1d9",
+    "url": "/js-calculator/static/js/runtime-main.dd4e1607.js"
+  }
+]);
