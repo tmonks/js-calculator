6876e0cc55c2ebd977a98405215d1e4b82b896b9
U-LCGH\tom.monks <tom.monks@LISDATA.LCGH.LOCAL>
2019-10-04 13:17:52 -0400

    Updates

--- precache-manifest.2e5aaa8ee29c515a397a2293aa4d2b35.js
+++ precache-manifest.2e5aaa8ee29c515a397a2293aa4d2b35.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1bc90bf29e9d7b7c4ff3895fcc13680e",
+    "revision": "26bf568d49d46c0b0692082cde2970e5",
     "url": "/js-calculator/index.html"
   },
   {
-    "revision": "9a1d9b47fd9b845594c9",
-    "url": "/js-calculator/static/css/main.ed1cb163.chunk.css"
+    "revision": "61a2f601f66c18f76361",
+    "url": "/js-calculator/static/css/main.e67893dc.chunk.css"
   },
   {
     "revision": "a4925379529007af90d1",
     "url": "/js-calculator/static/js/2.17d19fe2.chunk.js"
   },
   {
-    "revision": "9a1d9b47fd9b845594c9",
-    "url": "/js-calculator/static/js/main.a7088544.chunk.js"
+    "revision": "61a2f601f66c18f76361",
+    "url": "/js-calculator/static/js/main.9aeeddc0.chunk.js"
   },
   {
     "revision": "16dffeded061212ad1d9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/js-calculator/precache-manifest.357e14b42c19b341c190749fa4ddb334.js"
+  "/js-calculator/precache-manifest.2e5aaa8ee29c515a397a2293aa4d2b35.js"
 );
 
 self.addEventListener('message', (event) => {
